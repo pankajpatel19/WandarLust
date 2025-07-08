c27c1bff2fa37b72dce81671ea9bdbeeaddc09b2
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-07-08 17:43:38 +0530

    make responsive

--- app.js
+++ app.js
@@ -100,6 +100,9 @@ app.use((req, res, next) => {
 });
 
 //routes
+app.get("/", (req, res, next) => {
+  res.render("/listings");
+});
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
