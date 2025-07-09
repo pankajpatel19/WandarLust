0d9a4d8b3b29d22474c3e04f8123a86c27c05d15
Pankajpatel19 <pankajpatelinter@gmail.com>
2025-07-09 19:39:33 +0530

    fix home bar

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
 
 //routes
 app.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index.ejs");
 });
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewRouter);
